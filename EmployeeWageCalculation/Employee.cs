734e92cb485d9b926326639815fd0835b5ac9f4a
sohail-ahamed-qureshi <sohailqureshi82@gmail.com>
2021-05-26 11:52:05 +0530

    [sohail] Add: Calculation of Monthly wage

--- EmployeeWageCalculation/Employee.cs
+++ EmployeeWageCalculation/Employee.cs
@@ -10,38 +10,49 @@ namespace EmployeeWageCalculation
         {
             //constants
             const int WAGE_PER_HR = 20;
+            const int WORKING_DAYS = 20;
             const int IS_FULL_TIME=8;
             const int IS_PART_TIME = 4;
 
             //variables
             int dailyWage = 0;
-
+            int days = 0;
+            int isPresent;
+            int totalWage = 0;
             //random number generation
             Random rand = new Random();
-            int isPresent = rand.Next(0,3);
-
-            //using switch case
-            switch (isPresent)
+            //calculating for month
+            for(days =1; days <=WORKING_DAYS; days++)
             {
-                case 1:
-                    {
-                        dailyWage = WAGE_PER_HR * IS_FULL_TIME;
-                        Console.WriteLine($"Daily wage is {dailyWage}");
-                        break;
-                    }
-                case 2:
-                    {
-                        dailyWage = WAGE_PER_HR * IS_PART_TIME;
-                        Console.WriteLine($"Daily wage is {dailyWage}");
-                        break;
-                    }
-                default:
-                    {
-                        Console.WriteLine($"Daily wage is {dailyWage}");
-                        break;
-                    }
+                 isPresent = rand.Next(0, 3);
+
+                //using switch case
+                switch (isPresent)
+                {
+                    case 1:
+                        {
+                            dailyWage = WAGE_PER_HR * IS_FULL_TIME;
+                            Console.WriteLine($"Daily wage is {dailyWage}");
+                            break;
+                        }
+                    case 2:
+                        {
+                            dailyWage = WAGE_PER_HR * IS_PART_TIME;
+                            Console.WriteLine($"Daily wage is {dailyWage}");
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine($"Daily wage is {dailyWage}");
+                            break;
+                        }
+                }
+                totalWage += dailyWage;
             }
-                
+            Console.WriteLine($"Montly wage is {totalWage}");
+
+
+
         }
     }
 
