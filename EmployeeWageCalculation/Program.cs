ede6a65c2045515408d72a3bb6931ccb29cc42b0
sohail-ahamed-qureshi <sohailqureshi82@gmail.com>
2021-05-26 11:04:43 +0530

    [sohail] Refactor: Welcome message

--- EmployeeWageCalculation/Program.cs
+++ EmployeeWageCalculation/Program.cs
@@ -6,7 +6,7 @@ namespace EmployeeWageCalculation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Employee wage Calculation ");
         }
     }
 }
