55697716352df3925ad1ebeb20a5fb0d6fd71406
sohail-ahamed-qureshi <sohailqureshi82@gmail.com>
2021-05-26 11:29:30 +0530

    [sohail] Add: Calculation of Daily wage

--- EmployeeWageCalculation/Employee.cs
+++ EmployeeWageCalculation/Employee.cs
@@ -8,14 +8,28 @@ namespace EmployeeWageCalculation
     {
         public void AttendenceCheck()
         {
+            //constants
+            const int WAGE_PER_HR = 20;
+            const int IS_FULL_TIME=8;
+            //variables
+            int dailyWage = 0;
+
             //random number generation
             Random rand = new Random();
             int isPresent = rand.Next(2);
 
             if (isPresent == 1)
-                Console.WriteLine("Employee is Present.");
-            if (isPresent == 0)
-                Console.WriteLine("Employee is Absent");
+            {
+                dailyWage = WAGE_PER_HR * IS_FULL_TIME;
+                Console.WriteLine($"Daily wage is {dailyWage}");
+
+            }
+           else
+            {
+                Console.WriteLine($"Daily wage is {dailyWage}");
+            }
+                
         }
     }
+
 }
