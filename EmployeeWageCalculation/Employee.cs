968a3976b40b309aa4a8c63147dd7dd3b1bc486a
sohail-ahamed-qureshi <sohailqureshi82@gmail.com>
2021-05-26 12:07:59 +0530

    [sohail] Add: Calculating Monthly wage for total working hours

--- EmployeeWageCalculation/Employee.cs
+++ EmployeeWageCalculation/Employee.cs
@@ -13,19 +13,17 @@ namespace EmployeeWageCalculation
             const int WORKING_DAYS = 20;
             const int IS_FULL_TIME=8;
             const int IS_PART_TIME = 4;
-
             //variables
             int dailyWage = 0;
-            int days = 0;
-            int isPresent;
+            int days, isPresent;
             int totalWage = 0;
+            int totalHours=0;
             //random number generation
             Random rand = new Random();
             //calculating for month
             for(days =1; days <=WORKING_DAYS; days++)
-            {
+            {  
                  isPresent = rand.Next(0, 3);
-
                 //using switch case
                 switch (isPresent)
                 {
@@ -45,14 +43,17 @@ namespace EmployeeWageCalculation
                         {
                             Console.WriteLine($"Daily wage is {dailyWage}");
                             break;
-                        }
+                        }   
                 }
+                //checking total number of hours
+                totalHours += dailyWage / 20;
                 totalWage += dailyWage;
+                if (totalHours >= 100)
+                {
+                    break;
+                }
             }
-            Console.WriteLine($"Montly wage is {totalWage}");
-
-
-
+            Console.WriteLine($"Montly wage is {totalWage} and working hours is {totalHours}");
         }
     }
 
