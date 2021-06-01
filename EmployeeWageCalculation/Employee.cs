a3b8b560c22fd6f19fe66dc1f8b630a92bad9816
sohail-ahamed-qureshi <sohailqureshi82@gmail.com>
2021-06-02 00:02:57 +0530

    [sohail] Refactor: update to readable method

--- EmployeeWageCalculation/Employee.cs
+++ EmployeeWageCalculation/Employee.cs
@@ -9,32 +9,39 @@ namespace EmployeeWageCalculation
     public abstract class AttendenceCheck
     {
       public abstract  void Attendence();
+     public abstract void DailyWage(int attendence);
+
     }
     class Employee : AttendenceCheck
     {
         public override void Attendence()
         {
-            //constants
-            const int WAGE_PER_HR = 20;
-            const int IS_FULL_TIME=8;
-            //variables
-            int dailyWage = 0;
-
             //random number generation
             Random rand = new Random();
             int isPresent = rand.Next(2);
 
-            if (isPresent == 1)
+            DailyWage(isPresent);
+        }
+
+        public override void DailyWage(int isPresent)
+        {
+            //constants
+            const int WAGE_PER_HR = 20;
+            const int IS_FULL_TIME_HOURS = 8;
+            const int FULL_TIME = 1;
+            const int IS_ABSENT = 0;
+            //variables
+            int dailyWage = 0;
+            if (isPresent == FULL_TIME)
             {
-                dailyWage = WAGE_PER_HR * IS_FULL_TIME;
+                dailyWage = WAGE_PER_HR * IS_FULL_TIME_HOURS;
                 Console.WriteLine($"Daily wage is {dailyWage}");
 
             }
-           else
+            if (isPresent == IS_ABSENT)
             {
                 Console.WriteLine($"Daily wage is {dailyWage}");
             }
-                
         }
     }
 
